330dca87f892696605e726d43c473128d9ed0034
Ashwani Kumar Verma <a.kumar.verma@sap.com>
2023-08-14 20:47:49 +0530

    fix: openapi spec integrated

--- api/main.py
+++ api/main.py
@@ -1,19 +1,15 @@
-from flask import Flask, send_from_directory
+from flask import send_from_directory # Remove: import Flask
+import connexion
 import config
 
-
-app = Flask(__name__, static_folder='../ui/build')
-app.config['ENV'] = config.ENV
+app = connexion.App(__name__, specification_dir="./specs/", server_args={'static_folder': './../ui/build'})
+app.add_api("openapi.yaml")
+app.app.config['ENV'] = config.ENV
 
 @app.route('/', defaults={'path':'index.html'})
 @app.route('/<path:path>')
 def serve_static(path):
-  return send_from_directory(app.static_folder, path)
-
-
-@app.route('/api/v1/healthz')
-def healthz():
-  return "OK"
+  return send_from_directory(app.app.static_folder, path)
 
 if __name__ == '__main__':
   app.run(port=config.PORT, debug=config.DEBUG)
